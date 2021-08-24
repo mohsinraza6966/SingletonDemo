96677b754a87c1819ea0d4d1d4708258a20edf39
mohsinraza <mohsinraza@MOHSIN>
2021-08-24 21:37:05 +0500

    Add project files.

---
+++ Model/Singelton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo.Model
+{
+    public  sealed class Singelton
+    {
+        private static object Singeltonlock = new object();
+        private static Singelton Instance = null;
+        private static int Counter = 0;
+        public static Singelton GetInstance
+        {
+
+            get
+            {
+                lock (Singeltonlock)
+                {
+                    if (Instance == null)
+                    {
+                        Instance = new Singelton();
+                    }
+                }
+                return Instance;
+
+            }
+        }
+
+        private Singelton()
+        {
+            Counter++;
+            Console.WriteLine("Counter Value " + Counter.ToString());
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public void PrintInstanceCounter()
+        {
+            Console.WriteLine("Counter Value " + Counter.ToString());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SingletonDemo.Model;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singelton singeltonAdmin = Singelton.GetInstance;
+            singeltonAdmin.PrintDetails("Admin");
+            Singelton singeltonUser = Singelton.GetInstance;
+            singeltonUser.PrintDetails("User");
+            Singelton singeltonSuperAdmin = Singelton.GetInstance;
+            singeltonSuperAdmin.PrintDetails("Super Admin");
+            Singelton singeltonHR = Singelton.GetInstance;
+            singeltonHR.PrintDetails("HR");
+
+            singeltonHR.PrintInstanceCounter();
+            Console.ReadLine();
+        }
+    }
+}
